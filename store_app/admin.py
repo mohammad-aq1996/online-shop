f44d295c61f55c54db476a7812e96103abf970c7
Mohammad <mohammad.aq1996@outlook.com>
2022-03-24 20:41:41 +0430

    Modify store_app\admin: customize Brand and Category

--- store_app/admin.py
+++ store_app/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Product, Brand, ShoppingBasket, Category
 
 admin.site.register(Product)
-admin.site.register(Brand)
 admin.site.register(ShoppingBasket)
-admin.site.register(Category)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ['title']}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ['title']}
