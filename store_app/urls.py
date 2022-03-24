dcf92e33e81b8f5e8e6f49eaef48817407c77940
Mohammad <mohammad.aq1996@outlook.com>
2022-03-24 19:35:52 +0430

    add first migrate, add home template, add TemplateView for home template in store_app\urls.py

--- store_app/urls.py
+++ store_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.views.generic import TemplateView
 app_name = 'store_app'
 
 urlpatterns = [
-    path(),
+    path('', TemplateView.as_view(template_name='home.html')),
 ]
