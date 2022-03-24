55c657f47da7b3a90e430ad941baa8df746a74dd
Mohammad <mohammad.aq1996@outlook.com>
2022-03-24 19:59:43 +0430

    add logout and login in generic view

--- store/settings.py
+++ store/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-c(exm+*+$03z&b%w&ce=j%%hn56tkhoe2$f6x@h+60_t&+et45
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'store.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -112,13 +107,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -127,3 +120,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logout and Login
+
+LOGIN_REDIRECT_URL = 'store_app:home'
+LOGOUT_REDIRECT_URL = 'store_app:home'
--- store_app/urls.py
+++ store_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_view
+
 app_name = 'store_app'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login-view'),
+    path('logout/', auth_view.LogoutView.as_view(), name='logout-view'),
+
 ]
