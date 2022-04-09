b749cda7dae6c2308d12c5f258d4c1d18e07e2a5
mohammad <mohammad.aq1996@gmail.com>
2022-04-09 12:36:27 +0430

    add rest api views for products

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('laptop/list/', LaptopListAPIView.as_view()),
+    path('mobile/list/', MobileListAPIView.as_view()),
+    path('product/detail/<int:pk>/', ProductRetrieveUpdateDeleteAPIView.as_view()),
 ]
