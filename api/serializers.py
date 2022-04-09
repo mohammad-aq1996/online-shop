b6ec457565170e9f402d6a3928683e4de9449170
mohammad <mohammad.aq1996@gmail.com>
2022-04-09 11:59:12 +0430

    add serializer.py and urls.py in api

---
+++ api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from store_app.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    brand = serializers.StringRelatedField()
+
+    class Meta:
+        models = Product
+        fields = '__all__'
+
+
