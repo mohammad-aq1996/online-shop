91095a1aec5cc568cd9559acd04dbb191351e53f
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-19 11:05:30 +0430

    add test to accountapp

--- online_shop/accountapp/tests.py
+++ online_shop/accountapp/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class TestAccount(TestCase):
+    def test_create_user(self):
+        user = get_user_model().objects.create_user(
+            username = 'mohammad',
+            email = 'mohammad@yahoo.com',
+            password = 'Abc461379285',
+        )
+        self.assertEqual(user.username, 'mohammad')
+        self.assertEqual(user.email, 'mohammad@yahoo.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            username = 'mat',
+            email = 'mat@yahoo.com',
+            password = 'Xbc461379285',
+        )
+        self.assertEqual(user.username, 'mat')
+        self.assertEqual(user.email, 'mat@yahoo.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
